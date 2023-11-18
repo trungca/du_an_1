dbe8a8053caafff992778bb5b814cd69ad103570
bomongmo <giapntph33546@fpt.edu.vn>
2023-11-18 08:38:56 +0700

    2# Vu

--- admin/sanpham/dssp.php
+++ admin/sanpham/dssp.php
@@ -1,24 +1,25 @@
 <div class="breadcome-area">
-                <div class="container-fluid">
-                    <div class="row">
-                        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                            <div class="breadcome-list">
-                                <div class="row">
-                                    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
-                                        <div class="breadcomb-wp">
-											<div class="breadcomb-icon">
-												<i class="icon nalika-home"></i>
-											</div>
-											<div class="breadcomb-ctn">
-												<h2>Product Edit</h2>
-												<p>Welcome to Nalika <span class="bread-ntd">Admin Template</span></p>
-											</div>
-										</div>
-                                    </div>
-                                    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
-                                        <div class="breadcomb-report">
-											<button data-toggle="tooltip" data-placement="left" title="Download Report" class="btn"><i class="icon nalika-download"></i></button>
-										</div>
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                                <div class="breadcome-list">
+                                    <div class="row">
+                                        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                                            <div class="breadcomb-wp">
+                                                <div class="breadcomb-icon">
+                                                    <i class="icon nalika-home"></i>
+                                                </div>
+                                                <div class="breadcomb-ctn">
+                                                    <h2>Product Edit</h2>
+                                                    <p>Welcome to Nalika <span class="bread-ntd">Admin Template</span></p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                                            <div class="breadcomb-report">
+                                                <button data-toggle="tooltip" data-placement="left" title="Download Report" class="btn"><i class="icon nalika-download"></i></button>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
@@ -26,131 +27,75 @@
                     </div>
                 </div>
             </div>
-        </div>
-        <!-- Single pro tab start-->
-        <div class="single-product-tab-area mg-b-30">
-<div class="product-status mg-b-30">
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                        <div class="product-status-wrap">
-                            <h4>DANH SÁCH SẢN PHẨM</h4>
-                            <div class="add-product">
-                                <a href="index.php?act=themsp">THÊM SẢN PHẨM</a>
+            <!-- Single pro tab start-->
+            <div class="single-product-tab-area mg-b-30">
+    <div class="product-status mg-b-30">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                            <div class="product-status-wrap">
+                                <h4>DANH SÁCH SẢN PHẨM</h4>
+                                <div class="add-product">
+                                    <a href="index.php?act=dssp">DANH SÁCH SẢN PHẨM</a>
+                                </div>
+                                <form action="index.php?act=dssp" method="POST">
+                                        <input type="text" name="kyw">
+                                        <select name="iddm" id="">
+                                            <option value="0" selected >TẤT CẢ</option>
+                                        <?php
+                                            foreach($listdanhmuc as $danhmuc){
+                                                extract($danhmuc);
+                                                echo '<option value="'.$idsp.'">'.$name.'</option>';
+                                            }
+                                        ?>
+                                        </select>
+                                        <input type="submit" name="listok" value="GO">
+                                </form>
+                                <table>
+                                    <tr>
+                                        <th>ID</th>
+                                        <th>Hình</th>
+                                        <th>Tên sản phẩm</th>
+                                        <th>Số Lượng</th>
+                                        <th>Giá</th>
+                                        <th>Mô tả</th>
+                                        <th>Lượt xem</th>
+                                    </tr>
+                                    <?php
+                                    foreach($listsanpham as $sanpham){
+                                        extract($sanpham);
+                                        $suasp="index.php?act=suasp&idsp=".$idsp;
+                                        $xoasp="index.php?act=xoasp&idsp=".$idsp;
+                                        $hinhpath="../upload/".$image;
+                                        if(is_file($hinhpath)){
+                                            $image="<img src='".$hinhpath."' height='80'>";
+                                        }else{
+                                            $image="no photo";
+                                        }
+                                        echo'
+                                    <tr>
+                                    <td>'.$idsp.'</td>
+                                        <td>'.$image.'</td>
+                                        <td>'.$tensp.'</td>
+                                        <td>'.$soluong.'</td>
+                                        <td>'.$giasp.'</td>
+                                        <td>'.$mota.'</td>
+                                        <td>'.$luotxem.'</td>
+                                        <td>
+                                        <a href="'.$suasp.'"><button data-toggle="tooltip" title="Edit" class="pd-setting-ed"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></button>
+                                        <a href="'.$xoasp.'"><button data-toggle="tooltip" title="Trash" class="pd-setting-ed"><i class="fa fa-trash-o" aria-hidden="true"></i></button>
+                                        </td>
+                                    </tr>';
+
+                                    }
+                                    ?>
+                                    
+                                </table>
+                                
+                                
                             </div>
-                            <table>
-                                <tr>
-                                    <th>Hình</th>
-                                    <th>Tên sản phẩm</th>
-                                    <th>Trạng Thái</th>
-                                    <th>Số Lượng</th>
-                                    <th>Khuyến Mãi</th>
-                                    <th>Loại</th>
-                                    <th>Giá</th>
-                                    <th>Chức Năng</th>
-                                </tr>
-                                <tr>
-                                    <td><img src="img/new-product/5-small.jpg" alt="" /></td>
-                                    <td>Sản Phẩm 1</td>
-                                    <td>
-                                        <button class="pd-setting">Active</button>
-                                    </td>
-                                    <td>50</td>
-                                    <td>$750</td>
-                                    <td>Out Of Stock</td>
-                                    <td>$15</td>
-                                    <td>
-                                        <button data-toggle="tooltip" title="Edit" class="pd-setting-ed"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></button>
-                                        <button data-toggle="tooltip" title="Trash" class="pd-setting-ed"><i class="fa fa-trash-o" aria-hidden="true"></i></button>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td><img src="img/new-product/6-small.jpg" alt="" /></td>
-                                    <td>Sản Phẩm 2</td>
-                                    <td>
-                                        <button class="ps-setting">Paused</button>
-                                    </td>
-                                    <td>60</td>
-                                    <td>$1020</td>
-                                    <td>In Stock</td>
-                                    <td>$17</td>
-                                    <td>
-                                        <button data-toggle="tooltip" title="Edit" class="pd-setting-ed"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></button>
-                                        <button data-toggle="tooltip" title="Trash" class="pd-setting-ed"><i class="fa fa-trash-o" aria-hidden="true"></i></button>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td><img src="img/new-product/7-small.jpg" alt="" /></td>
-                                    <td>Sản Phẩm 3</td>
-                                    <td>
-                                        <button class="ds-setting">Disabled</button>
-                                    </td>
-                                    <td>70</td>
-                                    <td>$1050</td>
-                                    <td>Low Stock</td>
-                                    <td>$15</td>
-                                    <td>
-                                        <button data-toggle="tooltip" title="Edit" class="pd-setting-ed"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></button>
-                                        <button data-toggle="tooltip" title="Trash" class="pd-setting-ed"><i class="fa fa-trash-o" aria-hidden="true"></i></button>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td><img src="img/new-product/5-small.jpg" alt="" /></td>
-                                    <td>Sản Phẩm 4</td>
-                                    <td>
-                                        <button class="pd-setting">Active</button>
-                                    </td>
-                                    <td>120</td>
-                                    <td>$1440</td>
-                                    <td>In Stock</td>
-                                    <td>$12</td>
-                                    <td>
-                                        <button data-toggle="tooltip" title="Edit" class="pd-setting-ed"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></button>
-                                        <button data-toggle="tooltip" title="Trash" class="pd-setting-ed"><i class="fa fa-trash-o" aria-hidden="true"></i></button>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td><img src="img/new-product/6-small.jpg" alt="" /></td>
-                                    <td>Sản Phẩm 5</td>
-                                    <td>
-                                        <button class="pd-setting">Active</button>
-                                    </td>
-                                    <td>30</td>
-                                    <td>$540</td>
-                                    <td>Out Of Stock</td>
-                                    <td>$18</td>
-                                    <td>
-                                        <button data-toggle="tooltip" title="Edit" class="pd-setting-ed"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></button>
-                                        <button data-toggle="tooltip" title="Trash" class="pd-setting-ed"><i class="fa fa-trash-o" aria-hidden="true"></i></button>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td><img src="img/new-product/7-small.jpg" alt="" /></td>
-                                    <td>Sản Phẩm 6</td>
-                                    <td>
-                                        <button class="ps-setting">Paused</button>
-                                    </td>
-                                    <td>400</td>
-                                    <td>$4000</td>
-                                    <td>In Stock</td>
-                                    <td>$10</td>
-                                    <td>
-                                        <button data-toggle="tooltip" title="Edit" class="pd-setting-ed"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></button>
-                                        <button data-toggle="tooltip" title="Trash" class="pd-setting-ed"><i class="fa fa-trash-o" aria-hidden="true"></i></button>
-                                    </td>
-                                </tr>
-                            </table>
-                            <div class="custom-pagination">
-								<ul class="pagination">
-									<li class="page-item"><a class="page-link" href="#">Previous</a></li>
-									<li class="page-item"><a class="page-link" href="#">1</a></li>
-									<li class="page-item"><a class="page-link" href="#">2</a></li>
-									<li class="page-item"><a class="page-link" href="#">3</a></li>
-									<li class="page-item"><a class="page-link" href="#">Next</a></li>
-								</ul>
-                            </div>
+                            <a href="index.php?act=themsp"><input type="button" value="Them moi" style="color:white" class="btn btn-ctl-bt waves-effect waves-light"></a>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
